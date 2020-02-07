c097b5a93bf06471a87f0e9e63acec0b73df8f8c
Nicolas Salvi <nicolas.salvi@reperrant.net>
2020-02-07 11:37:19 +0100

    Grid OK

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// 0 est un mur, 1 c'est le sol, 2 c'est un bonbon 
+var grille = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], 
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0 ], 
+    [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0 ],
+    [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0 ],
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0 ], 
+    [0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0 ],
+    [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0 ],
+    [0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0 ],
+    [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0 ],
+    [0, 0, 0, 0, 2, 0, 2, 0, 0, 1, 0, 0, 2, 0, 2, 0, 0, 0, 0 ],
+    [2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2 ],
+    [0, 0, 0, 0, 2, 0, 2, 0, 0, 1, 0, 0, 2, 0, 2, 0, 0, 0, 0 ],
+    [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0 ],
+    [0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0 ],
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0 ],
+    [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0 ],
+    [0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0 ],
+    [0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0 ],
+    [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0 ],
+    [0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0 ],
+    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0 ], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+
+]
+
+function initGrille() {
+   let _grille = document.querySelector('#grille');
+   _grille.innerHTML = "";
+   _grille.style.gridTemplateColumns = 'repeat('+grille[0].length+', 2vw)';
+   _grille.style.gridTemplateRows = 'repeat('+grille.length+', 2vw)';
+
+    for(let ligne in grille) {
+            grille[ligne];
+        
+        for(let col in grille[ligne]) {
+           let cellule = grille[ligne][col];
+           let monElem = document.createElement("div");
+           if(cellule == 0) {
+            monElem.classList.add("mur");
+        } else if(cellule == 1) {
+            monElem.classList.add("sol");
+        } else if(cellule == 2) {
+            monElem.classList.add("bonbon");
+        }
+        monElem.style.gridRow = (+ligne) + 1;
+        monElem.style.gridColumn = (+col) + 1;
+
+            _grille.append(monElem);
+        };
+    };
+}
+
+initGrille();
+
+//nombre de colonnes : grille[0].length
+//nombre de lignes : grille.length
+
+
+//setTimeout si vous faites du récursif
