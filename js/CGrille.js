094eb7b99cd57712ead0f6199efc727b9e4276c5
Nicolas Salvi <nicolas.salvi@reperrant.net>
2020-02-12 16:09:27 +0100

    refactorisation grille

---
+++ js/CGrille.js
@@ -0,0 +1,62 @@
+class Grille {
+    grilleDef;
+    constructor(){
+        this.grilleDef = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], 
+            [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0 ], 
+            [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0 ],
+            [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0 ],
+            [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0 ], 
+            [0, 2, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 0 ],
+            [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0 ],
+            [0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0 ],
+            [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 2, 2, 3 ],
+            [0, 0, 0, 0, 2, 0, 2, 0, 0, 1, 0, 0, 2, 0, 2, 0, 0, 0, 0 ],
+            [2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2 ],
+            [0, 0, 0, 0, 2, 0, 2, 0, 0, 1, 0, 0, 2, 0, 2, 0, 0, 0, 0 ],
+            [0, 1, 1, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 0, 1, 1, 0 ],
+            [0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0 ],
+            [0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0 ],
+            [0, 2, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2, 0 ],
+            [0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0 ],
+            [0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0 ],
+            [0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 0 ],
+            [3, 2, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0 ],
+            [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0 ], 
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+        
+        ]
+    }
+    getGrilleDef() {
+        return this.grilleDef;
+    }
+    initGrille(grille) {
+        _grille.innerHTML = "";
+        _grille.style.gridTemplateColumns = 'repeat('+grille[0].length+', 2vw)';
+        _grille.style.gridTemplateRows = 'repeat('+grille.length+', 2vw)';
+     
+         for(let ligne in grille) {
+            grille[ligne];
+             for(let col in grille[ligne]) {
+                let cellule = grille[ligne][col];
+                let monElem = document.createElement("div");
+     
+                if(cellule == 0) {
+                 monElem.classList.add("mur");
+             } else if(cellule == 1) {
+                 monElem.classList.add("sol");
+             } else if(cellule == 2) {
+                 monElem.classList.add("bonbon");
+             } else if(cellule == 3) {
+                 monElem.classList.add("mur2");
+             }
+             monElem.style.gridRow = (+ligne) + 1;
+             monElem.style.gridColumn = (+col) + 1;
+     
+                 _grille.append(monElem);
+             };
+         };    
+         }
+   
+}
+
